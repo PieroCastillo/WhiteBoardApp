29956504d4722e537c80392170f925a803f30ad4
Piero Castillo <pierocastillo2005@gmail.com>
2022-01-19 17:29:03 -0500

    Agregar archivos de proyecto.

---
+++ WhiteBoardApp/WhiteBoard.Properties.cs
@@ -0,0 +1,96 @@
+﻿using Avalonia;
+using Avalonia.Media;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhiteBoardApp
+{
+    public partial class WhiteBoard
+    {
+
+        private double _Thickness = 5;
+        public double Thickness
+        {
+            get => _Thickness;
+            set => SetAndRaise(ThicknessProperty, ref _Thickness, value);
+        }
+
+        public static readonly DirectProperty<WhiteBoard, double> ThicknessProperty =
+            AvaloniaProperty.RegisterDirect<WhiteBoard, double>(nameof(Thickness), o => o.Thickness, (o, v) => o.Thickness = v, 5);
+
+
+        private int _MiterLimit  = 20;
+        public int MiterLimit
+        {
+            get => _MiterLimit;
+            set => SetAndRaise(MiterLimitProperty, ref _MiterLimit, value);
+        }
+
+        public static readonly DirectProperty<WhiteBoard, int> MiterLimitProperty =
+            AvaloniaProperty.RegisterDirect<WhiteBoard, int>(nameof(MiterLimit), o => o.MiterLimit, (o, v) => o.MiterLimit = v, 10);
+
+        private PenLineCap _LineCap = PenLineCap.Round;
+        public PenLineCap LineCap
+        {
+            get => _LineCap;
+            set => SetAndRaise(LineCapProperty, ref _LineCap, value);
+        }
+
+        public static readonly DirectProperty<WhiteBoard, PenLineCap> LineCapProperty =
+            AvaloniaProperty.RegisterDirect<WhiteBoard, PenLineCap>(nameof(LineCap), o => o.LineCap, (o, v) => o.LineCap = v);
+
+        private PenLineJoin _LineJoin = PenLineJoin.Bevel;
+        public PenLineJoin LineJoin
+        {
+            get => _LineJoin;
+            set => SetAndRaise(LineJoinProperty, ref _LineJoin, value);
+        }
+
+        public static readonly DirectProperty<WhiteBoard, PenLineJoin> LineJoinProperty =
+            AvaloniaProperty.RegisterDirect<WhiteBoard, PenLineJoin>(nameof(LineJoin), o => o.LineJoin, (o, v) => o.LineJoin = v);
+
+        private IDashStyle _LineStyle = DashStyle.DashDotDot;
+        public IDashStyle LineStyle
+        {
+            get => _LineStyle;
+            set => SetAndRaise(LineStyleProperty, ref _LineStyle, value);
+        }
+
+        public static readonly DirectProperty<WhiteBoard, IDashStyle> LineStyleProperty =
+            AvaloniaProperty.RegisterDirect<WhiteBoard, IDashStyle>(nameof(LineStyle), o => o.LineStyle, (o, v) => o.LineStyle = v, DashStyle.DashDotDot);
+
+        private IBrush _Brush = Brushes.Red;
+        public IBrush Brush
+        {
+            get => _Brush;
+            set => SetAndRaise(BrushProperty, ref _Brush, value);
+        }
+
+        public static readonly DirectProperty<WhiteBoard, IBrush> BrushProperty =
+            AvaloniaProperty.RegisterDirect<WhiteBoard, IBrush>(nameof(Brush), o => o.Brush, (o, v) => o.Brush = v, Brushes.Black);
+
+        public IBrush Background
+        {
+            get => GetValue(BackgroundProperty);
+            set => SetValue(BackgroundProperty, value);
+        }
+
+        public static readonly StyledProperty<IBrush> BackgroundProperty =
+            AvaloniaProperty.Register<WhiteBoard, IBrush>(nameof(Background), Brushes.White);
+
+
+        public bool IsErasing
+        {
+            get => GetValue(IsErasingProperty);
+            set => SetValue(IsErasingProperty, value);
+        }
+
+        public static readonly StyledProperty<bool> IsErasingProperty =
+            AvaloniaProperty.Register<WhiteBoard, bool>(nameof(IsErasing), false);
+
+
+    }
+}
---
+++ WhiteBoardApp/WhiteBoard.cs
@@ -0,0 +1,109 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Shapes;
+using Avalonia.Input;
+using Avalonia.LogicalTree;
+using Avalonia.Media;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+using Avalonia.Rendering.SceneGraph;
+using Avalonia.Skia;
+using Avalonia.Threading;
+using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhiteBoardApp
+{
+    enum FrameState
+    {
+        Nothing,
+        Draw,
+        Erase
+    }
+
+    public partial class WhiteBoard : Control
+    {
+        bool isDrawing = false;
+        FrameState state;
+        WriteableBitmap bitmap;
+        SKSurface surface;
+        SKCanvas canvas;
+        Point lastPoint;
+
+        public override void EndInit()
+        {
+            base.EndInit();
+
+            bitmap = new(new PixelSize((int)Width, (int)Height), new Vector(96, 96), PixelFormat.Rgba8888, AlphaFormat.Premul);
+            using(var lockedBitmap = bitmap.Lock())
+            {
+                var info = new SKImageInfo(lockedBitmap.Size.Width, lockedBitmap.Size.Height, lockedBitmap.Format.ToSkColorType());
+
+                surface = SKSurface.Create(info, lockedBitmap.Address, lockedBitmap.RowBytes);
+                canvas = surface.Canvas;
+                canvas.Clear(SKColors.White);
+            }
+        }
+
+        protected override void OnPointerPressed(PointerPressedEventArgs e)
+        {
+            base.OnPointerPressed(e);
+
+            state = IsErasing ? FrameState.Erase : FrameState.Draw;
+            lastPoint = e.GetCurrentPoint(this).Position;
+            e.Handled = true;
+        }
+
+        protected override void OnPointerMoved(PointerEventArgs e)
+        {
+            base.OnPointerMoved(e);
+            var currentPoint = e.GetCurrentPoint(this);
+
+            switch (state)
+            {
+                case FrameState.Draw:
+                    using (var paint = new SKPaint())
+                    {
+                        paint.Shader = SKShader.CreateColor(SKColors.Gray);
+                        paint.StrokeWidth = 10;
+                        paint.IsAntialias = true;
+                        paint.StrokeCap = SKStrokeCap.Round;
+                        paint.StrokeJoin = SKStrokeJoin.Round;
+                        canvas.DrawLine(lastPoint.ToSKPoint(), currentPoint.Position.ToSKPoint(), paint);
+                    }
+                    break;
+
+                case FrameState.Erase:
+                    using (var paint = new SKPaint())
+                    {
+                        paint.Shader = SKShader.CreateColor(SKColors.White);
+                        canvas.DrawCircle(currentPoint.Position.ToSKPoint(), 5, paint);
+                    }
+                    break;
+            }
+            lastPoint = currentPoint.Position;
+            Dispatcher.UIThread.InvokeAsync(InvalidateVisual, DispatcherPriority.Background);
+            e.Handled = true;
+        }
+
+        protected override void OnPointerReleased(PointerReleasedEventArgs e)
+        {
+            base.OnPointerReleased(e);
+            var currentPoint = e.GetCurrentPoint(this);
+            state = FrameState.Nothing;
+
+            Dispatcher.UIThread.InvokeAsync(InvalidateVisual, DispatcherPriority.Background);
+            e.Handled = true;
+        }
+
+        public override void Render(DrawingContext context)
+        {
+            context.DrawImage(bitmap, new Rect(0, 0, Bounds.Width, Bounds.Height));
+        }
+    }
+}
